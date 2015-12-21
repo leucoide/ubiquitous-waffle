eb0fb357001a8192009e51ca40f5975263ee2dad
Felipe Ribeiro <felipe.ribeiro.santos@tvglobo.com>
2015-12-21 20:54:45 -0200

    Removing status bar...

--- app/src/main/java/com/globo/thevoicelights/MainActivity.java
+++ app/src/main/java/com/globo/thevoicelights/MainActivity.java
@@ -1,21 +1,27 @@
 package com.globo.thevoicelights;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.WindowManager;
+
+import static android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int mDelay = 500;
+    private final Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("NATIVE", this.stringFromJNI());
-    }
 
-    public native String stringFromJNI();
-
-    static {
-        System.loadLibrary("thevoicelights");
+        if (Build.VERSION.SDK_INT < 16) {
+            getWindow().setFlags(FLAG_FULLSCREEN,
+                    FLAG_FULLSCREEN);
+        }
     }
 }
