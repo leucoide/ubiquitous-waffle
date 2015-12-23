f59c2f627361fe213191f70d6c65d4a6e8265656
Yuri Vasquez <yuri.vasquez91@gmail.com>
2015-12-23 12:53:30 -0200

    removed random stub

--- app/src/main/jni/thevoicelights.c
+++ app/src/main/jni/thevoicelights.c
@@ -19,24 +19,6 @@ int dmxR = 0;
 int dmxG = 0;
 int dmxB = 0;
 
-long random_at_most(long max) {
-    unsigned long
-    // max <= RAND_MAX < ULONG_MAX, so this is okay.
-            num_bins = (unsigned long) max + 1,
-            num_rand = (unsigned long) RAND_MAX + 1,
-            bin_size = num_rand / num_bins,
-            defect = num_rand % num_bins;
-
-    long x;
-    do {
-        x = random();
-    }
-        // This is carefully written not to overflow
-    while (num_rand - defect <= (unsigned long) x);
-
-    // Truncated division is intentional
-    return x / bin_size;
-}
 
 int receiver(artnet_node node, void *pp, void *d) {
     artnet_packet pack = (artnet_packet) pp;
